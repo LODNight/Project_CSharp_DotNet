cccaf40bf909aba3d779eade263ae647144d7895
LODNight <khuunhattin123@gmail.com>
2023-03-05 17:57:57 +0700

    Add project files.

---
+++ Login_Project/Login_Project/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Login_Project.Controllers;
+[Route("home")]
+public class HomeController : Controller
+{
+    [Route("index")]
+    [Route("")]
+    [Route("~/")]
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
