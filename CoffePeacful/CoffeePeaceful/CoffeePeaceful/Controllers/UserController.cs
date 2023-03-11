7756e87242700e9f2f3fe0e7a31ac90a05475c10
phamphucloi <phamphucloi460@gmail.com>
2023-03-11 22:13:11 +0700

    aa

---
+++ CoffePeacful/CoffeePeaceful/CoffeePeaceful/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeePeaceful.Controllers;
+[Route("user")]
+public class UserController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
